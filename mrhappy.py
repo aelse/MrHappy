40cd9f5c7c4a64fad90c236624ac1d6c6b96bac9
Alexander Else <aelse@else.id.au>
2011-10-03 13:33:57 +1100

    added config generation option

--- mrhappy.py
+++ mrhappy.py
@@ -3,15 +3,42 @@ import sys
 def parse_options():
     import optparse
     parser = optparse.OptionParser()
+    parser.add_option("-v", "--verbose", dest="verbose", default=False,
+        action="store_true",
+        help="print verbose status information")
     parser.add_option("-c", "--config", dest="config_file", metavar="FILE",
         help="bot configuration file")
-    parser.add_option("-v", "--verbose", dest="verbose", default=False,
-        help="print verbose status information")
+    parser.add_option("-g", "--gen-config", dest="gen_config", default=False,
+        action="store_true",
+        help="generate a bot configuration file")
     (options, args) = parser.parse_args()
     return options
 
+def gen_config():
+    """
+    Generate a sample configuration object.
+    """
+    from ConfigParser import SafeConfigParser
+    config = SafeConfigParser()
+    config.add_section('General')
+    config.set('General', 'nick', 'MrHappy')
+    config.set('General', 'name', 'MrHappy, the Python bot.')
+    config.set('General', 'nickpass', 'mypass')
+    config.add_section('Server')
+    config.set('Server', 'host', 'localhost')
+    config.set('Server', 'port', '6667')
+    config.set('Server', 'retry_interval', '60')
+    config.add_section('Channel')
+    config.set('Channel', 'a', '#MrHappy\'s_Wild_Ride')
+    config.set('Channel', 'b', '#testchan')
+    return config
+
 def main():
     options = parse_options()
+    if options.gen_config:
+        config = gen_config()
+        config.write(sys.stdout)
+        return 0
     return 0
 
 if __name__ == "__main__":
