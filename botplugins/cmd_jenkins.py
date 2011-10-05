a93de54957caf2f575ed78d6f1e1568027bfb316
Alexander Else <aelse@else.id.au>
2011-10-05 17:32:40 +1100

    added jenkins build reporting

---
+++ botplugins/cmd_jenkins.py
@@ -0,0 +1,90 @@
+from botplugin import BotPlugin
+import xml.dom.minidom
+import urllib
+import re
+
+class Jenkins(BotPlugin):
+
+    def setup(self):
+        self.jenkins_url = 'http://jenkins'
+        self.proj_matcher = re.compile('[\w\d\-_\.]+')
+        # limit lines of information to be reported for a request
+        self.status_msg_limit = 5
+
+    def command_jenkins(self, bot, e, command, arguments, channel, nick):
+        arguments = arguments.strip()
+
+        # Provide help information if no sub-command given
+        if len(arguments) == 0:
+            arguments = 'help'
+
+        # work out the given sub-command and arguments
+        if arguments.find(' ') > -1:
+            cmd, args = arguments.split(' ', 1)
+        else:
+            cmd, args = arguments, ''
+
+        f = None
+        try:
+            f = getattr(self, 'cmd_' + cmd)
+        except AttributeError:
+            pass
+
+        if f:
+            message = f(args)
+            if type(message) == type(''):
+                message = [message]
+            for line in message:
+                bot.reply(line, channel, nick)
+        else:
+            bot.reply('Bad arguments \'%s\'' % (arguments), channel, nick)
+
+    def cmd_help(self, args):
+        help = [
+            'help', ' - Print this message',
+            'buildstatus <project>', ' - Report on project build',
+        ]
+        return help 
+
+    def cmd_buildstatus(self, proj_name):
+        if not self.proj_matcher.match(proj_name):
+            return 'Badly formatted project %s' % proj_name
+        return gen_build_info(proj_name, self.status_msg_limit)
+
+
+def fetch_url(url):
+    data = None
+    try:
+        response = urllib.urlopen(url)
+        if response.getcode() == 200:
+            data = response.read()
+        response.close()
+    except:
+        pass
+    return data
+
+def gen_build_info(proj_name, limit=-1):
+    url = 'http://jenkins/job/%s/rssAll' % proj_name
+    rss = fetch_url(url)
+    if not rss:
+        return ['Unable to fetch build information']
+
+    xml_data = xml.dom.minidom.parseString(rss)
+
+    # locate build entries
+    entries = xml_data.getElementsByTagName('entry')
+
+    count = 0
+    buildstatus = ['== Builds for %s ==' % proj_name]
+    for e in entries:
+        updated = e.getElementsByTagName('updated')[0].lastChild.nodeValue
+        title = e.getElementsByTagName('title')[0].lastChild.nodeValue
+        buildstatus.append('%s: %s' % (updated, title))
+        count += 1
+        if count == limit:
+            break
+    return buildstatus
+
+if __name__ == '__main__':
+    for line in gen_build_info('bpl-pull-queue'):
+        print line
