ffa7bccd1434a26883de54af38bff403f35f1812
Alexander Else <aelse@else.id.au>
2011-10-25 10:28:15 +1100

    iterate over list copy as operation mangles list

--- botplugins/cmd_plugins.py
+++ botplugins/cmd_plugins.py
@@ -5,7 +5,7 @@ class PluginOperations(BotPlugin):
     def command_unload(self, bot, e, command, args, channel, nick):
         if args == 'plugins':
             bot.reply('Unloading all plugins.', channel, nick)
-            for plugin in bot.plugins:
+            for plugin in list(bot.plugins):
                 if plugin != self:
                     bot.unload_plugin(plugin)
             #bot.unload_plugin(self)
