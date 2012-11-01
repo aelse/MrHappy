bf2e44c65bc4ec425e893b34a29e44b7c584c179
Alexander Else <aelse@else.id.au>
2012-11-01 15:45:32 +1100

    pep8 changes

--- mrhappy.py
+++ mrhappy.py
@@ -32,6 +32,8 @@ import botcommon
 from botplugins.botplugin import BotPlugin
 from pprint import PrettyPrinter
 
+from pinder import Campfire
+
 version = "0.1"
 
 # Defaults for bot configuration. Only contains entries where a default
@@ -49,7 +51,7 @@ conf = {
     },
 }
 
-from pinder import Campfire
+
 class CampfireConnection():
     connection = None
     subdomain = None
@@ -84,6 +86,7 @@ class CampfireConnection():
             print 'Oh no, caught an exception talking to the room'
         self.room.leave()
 
+
 # construct event object for campfire messages to simulate functionality
 # of the IRC module.
 class CampfireEvent():
@@ -91,7 +94,7 @@ class CampfireEvent():
         self.msg = msg
 
     def arguments(self):
-        return [ self.msg ]
+        return [self.msg]
 
     def eventtype(self):
         return 'pubmsg'
@@ -101,7 +104,8 @@ class CampfireEvent():
 
 
 class MrHappyBot(object):
-    def __init__(self, token, campfire_prefix, room, nick, name, nickpass=None, recon=60):
+    def __init__(self, token, campfire_prefix, room, nick, name,
+                 nickpass=None, recon=60):
         self.connection = CampfireConnection(campfire_prefix, token, room)
         self.queue = botcommon.OutputManager(self.connection, .2)
         self.queue.start()
@@ -119,7 +123,7 @@ class MrHappyBot(object):
             return
         e = CampfireEvent(msg['body'])
         uid = msg['user_id']
-        if not self.cfusers.has_key(uid):
+        if not uid in self.cfusers:
             try:
                 u = self.connection.connection.user(uid)
                 self.cfusers[uid] = u['user']['name']
@@ -133,7 +137,8 @@ class MrHappyBot(object):
             from_nick = 'Oops'
 
         info('Received from %s: %s' % (from_nick, msg['body']))
-        m = re.match('%s[:,](.*)' % self.nickname, e.arguments()[0], re.IGNORECASE)
+        m = re.match('%s[:,](.*)' % self.nickname, e.arguments()[0],
+                     re.IGNORECASE)
         if m:
             cmd = m.groups()[0]
             info('Command: %s' % cmd)
@@ -145,7 +150,7 @@ class MrHappyBot(object):
         self.connection.connect()
         try:
             self.room.speak('Exception: %s' % ex)
-            self.room.speak('Please let me live!');
+            self.room.speak('Please let me live!')
             self.room.join()
         except:
             print 'Oh no, caught an exception talking to the room'
@@ -164,7 +169,8 @@ class MrHappyBot(object):
     def load_modules(self):
         modules = discover_modules(self.conf['plugindir'])
         for m in modules:
-            modname = string.join((self.conf['plugindir'].replace('/', '.'), m), '.')
+            mpath = self.conf['plugindir'].replace('/', '.')
+            modname = string.join((mpath, m), '.')
             self.load_plugin(modname)
 
     def setup_plugins(self):
@@ -174,14 +180,15 @@ class MrHappyBot(object):
             try:
                 plugin_config = self.conf[section_name]
             except KeyError:
-                debug('No configuration section \'%s\' for %s' % (section_name, p.module_barename))
+                debug('No configuration section \'%s\' for %s' % (
+                      section_name, p.module_barename))
             p.setup(self, plugin_config)
 
     def load_plugin(self, modname):
         debug('Inspecting %s' % modname)
         plugin = __import__(modname)
         d = plugin.__dict__
-        barename = modname[modname.rfind('.')+1:]
+        barename = modname[modname.rfind('.') + 1:]
         d = d[barename].__dict__
 
         discovered = []
@@ -208,12 +215,14 @@ class MrHappyBot(object):
     def on_privmsg(self, c, e):
         debug('Received private message')
         from_nick = e.source()
-        self.do_command(e, string.strip(e.arguments()[0]), string.strip(from_nick))
+        self.do_command(e, string.strip(e.arguments()[0]),
+                        string.strip(from_nick))
 
     def on_pubmsg(self, c, e):
         debug('Received public message')
         from_nick = e.source()
-        m = re.match('%s[:,](.*)' % self.nickname, e.arguments()[0], re.IGNORECASE)
+        m = re.match('%s[:,](.*)' % self.nickname, e.arguments()[0],
+                     re.IGNORECASE)
         if m:
             cmd = m.groups()[0]
             self.do_command(e, string.strip(cmd), string.strip(from_nick))
@@ -276,28 +285,30 @@ class MrHappyBot(object):
                 f(self, e, command, args, channel, nick)
         #self.reply('Responding to direct request.', channel, nick)
 
+
 def parse_options():
     import optparse
     parser = optparse.OptionParser()
     parser.add_option("-v", "--verbose", dest="verbose", default=False,
-        action="store_true",
-        help="print verbose status information")
+                      action="store_true",
+                      help="print verbose status information")
     parser.add_option("-d", "--debug", dest="debug", default=False,
-        action="store_true",
-        help="print debugging information")
+                      action="store_true",
+                      help="print debugging information")
     parser.add_option("-l", "--logfile", dest="log_file", metavar="FILE",
-        help="log to file")
+                      help="log to file")
     parser.add_option("-c", "--config", dest="config_file", metavar="FILE",
-        default="mrhappy.cfg",
-        help="bot configuration file")
+                      default="mrhappy.cfg", help="bot configuration file")
     parser.add_option("-g", "--gen-config", dest="gen_config", default=False,
-        action="store_true",
-        help="generate a bot configuration file")
+                      action="store_true",
+                      help="generate a bot configuration file")
     parser.add_option("-p", "--plugins", dest="load_plugins", default=False,
-        action="store_true",
-        help="load plugins (or include plugin options in --gen-config)")
+                      action="store_true",
+                      help="load plugins (or include plugin options in "
+                      "--gen-config)")
     (options, args) = parser.parse_args()
     return options
+
 
 def process_config(filename):
     from ConfigParser import SafeConfigParser
@@ -309,9 +320,10 @@ def process_config(filename):
     for section in parser.sections():
         for (name, value) in parser.items(section):
             debug('%s => %s = %s' % (section, name, value))
-            if not conf.has_key(section):
+            if not section in conf:
                 conf[section] = {}
             conf[section][name] = value
+
 
 def gen_config(load_plugins):
     """
@@ -336,7 +348,11 @@ def gen_config(load_plugins):
         # Instantiate a bot with skeleton config and load plugins
         global conf
         server = (conf['Server']['host'], int(conf['Server']['port']))
-        bot = MrHappyBot(conf['Server']['token'], conf['Server']['campfire_prefix'], conf['Server']['room'], conf['General']['nick'], conf['General']['name'])
+        bot = MrHappyBot(conf['Server']['token'],
+                         conf['Server']['campfire_prefix'],
+                         conf['Server']['room'],
+                         conf['General']['nick'],
+                         conf['General']['name'])
         bot.conf = conf
 
         bot.load_modules()
@@ -353,6 +369,7 @@ def gen_config(load_plugins):
                 config.set(section_name, o, v)
     return config
 
+
 def discover_modules(directory):
     modules = []
     for _, _, files in os.walk(directory):
@@ -361,6 +378,7 @@ def discover_modules(directory):
                 modname = f[:f.find('.')]
                 modules.append(modname)
     return modules
+
 
 def main():
     global conf
@@ -379,7 +397,8 @@ def main():
         loglevel = getattr(logging, 'DEBUG')
 
     if options.log_file:
-        logging.basicConfig(filename=options.log_file, level=loglevel, format=logformat)
+        logging.basicConfig(filename=options.log_file,
+                            level=loglevel, format=logformat)
     else:
         logging.basicConfig(level=loglevel, format=logformat)
 
@@ -394,7 +413,11 @@ def main():
     process_config(options.config_file)
 
     server = (conf['Server']['host'], int(conf['Server']['port']))
-    bot = MrHappyBot(conf['Server']['token'], conf['Server']['campfire_prefix'], conf['Server']['room'], conf['General']['nick'], conf['General']['name'])
+    bot = MrHappyBot(conf['Server']['token'],
+                     conf['Server']['campfire_prefix'],
+                     conf['Server']['room'],
+                     conf['General']['nick'],
+                     conf['General']['name'])
     bot.conf = conf
 
     if options.load_plugins:
@@ -408,6 +431,7 @@ def main():
         except KeyboardInterrupt:
             info('Received ctrl-c')
             bot.shutdown("Terminating")
+
         except Exception, e:
             logging.exception(e)
             #bot.shutdown("Exception")
